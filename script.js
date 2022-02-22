422cd35ece132d51e1e78eb13c72f6bd053a3044
michaelsatumba <mjsatumba@gmail.com>
2022-02-22 15:25:04 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// alert('hello');
+
+// function sound() {
+// 	alert('sound');
+// }
+
+const audioA = new Audio('sounds/bassDrum.mp3');
+const audioS = new Audio('sounds/snareDrum.mp3');
+const audioD = new Audio('sounds/drumSticks.mp3');
+const audioF = new Audio('sounds/mediumTomDrum.mp3');
+const audioG = new Audio('sounds/china.mp3');
+const audioH = new Audio('sounds/hiHat.mp3');
+const audioJ = new Audio('sounds/.mp3');
+const audioK = new Audio('sounds/kickDrum.mp3');
+const audioL = new Audio('sounds/.mp3');
+
+function soundA() {
+	audioA.play();
+}
+
+function soundS() {
+	audioS.play();
+}
+
+function soundD() {
+	audioD.play();
+}
+
+function soundF() {
+	audioF.play();
+}
+
+function soundG() {
+	audioG.play();
+}
+
+function soundH() {
+	audioH.play();
+}
+
+function soundJ() {
+	audioJ.play();
+}
+
+function soundK() {
+	audioK.play();
+}
+
+function soundL() {
+	audioL.play();
+}
