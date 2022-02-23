7cff3694eb9da2d4a53384ffa37740546dc47539
michaelsatumba <mjsatumba@gmail.com>
2022-02-22 17:05:06 -0800

    rounded border - finish sounds - change gradient

--- script.js
+++ script.js
@@ -10,9 +10,47 @@ const audioD = new Audio('sounds/drumSticks.mp3');
 const audioF = new Audio('sounds/mediumTomDrum.mp3');
 const audioG = new Audio('sounds/china.mp3');
 const audioH = new Audio('sounds/hiHat.mp3');
-const audioJ = new Audio('sounds/.mp3');
+const audioJ = new Audio('sounds/gong.mp3');
 const audioK = new Audio('sounds/kickDrum.mp3');
-const audioL = new Audio('sounds/.mp3');
+const audioL = new Audio('sounds/scratch.mp3');
+
+window.addEventListener('keydown', (event) => {
+	// if (event.key === 's') {
+	// 	soundS();
+	// }
+	switch (event.key) {
+		case 'a':
+			soundA();
+			break;
+		case 's':
+			soundS();
+			break;
+		case 'd':
+			soundD();
+			break;
+		case 'f':
+			soundF();
+			break;
+		case 'g':
+			soundG();
+			break;
+		case 'h':
+			soundH();
+			break;
+		case 'j':
+			soundJ();
+			break;
+		case 'k':
+			soundK();
+			break;
+		case 'l':
+			soundL();
+			break;
+
+		default:
+			break;
+	}
+});
 
 function soundA() {
 	audioA.play();
